6303baccaae2da578421e6161e8fb88ec2beccb1
majovanilla <mariajose.reyes@wizeline.com>
2022-07-12 10:40:11 -0500

    Improve Slider render

--- src/components/organisms/Slider/Slider.jsx
+++ src/components/organisms/Slider/Slider.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+// Atoms
+import Loader from '../../atoms/Loader/Loader';
+
 // Styled components
 import {
-  Container, BannerImage, Loader, Texts, Arrow,
+  Container, BannerImage, Texts, Arrow,
 } from './styled';
 
 // Assets
 import backArrow from '../../../images/back-arrow.png';
 import nextArrow from '../../../images/forward-arrow.png';
 
-function Slider({ bannerData, isLoading }) {
+function Slider({ bannerData: { results }, isLoading }) {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const changeSlide = (action) => {
-    const { length } = bannerData.results;
+    const { length } = results;
     console.log('length', length);
     if (action === 'next') {
       if (currentSlide === length - 1) {
@@ -35,17 +38,17 @@ function Slider({ bannerData, isLoading }) {
   return (
     isLoading ? <Loader />
       : (
-        <Container key={bannerData.results[0].id}>
+        <Container>
           <BannerImage
-            src={bannerData.results[currentSlide].data.main_image.url}
-            alt={bannerData.results[currentSlide].data.main_image.alt}
+            src={results[currentSlide].data.main_image.url}
+            alt={results[currentSlide].data.main_image.alt}
           >
             <Arrow src={backArrow} className="left" onClick={() => changeSlide('prev')} />
             <Arrow src={nextArrow} className="right" onClick={() => changeSlide('next')} />
           </BannerImage>
           <Texts>
-            <p>{bannerData.results[currentSlide].data.title}</p>
-            <p>{bannerData.results[currentSlide].data.description[0].text}</p>
+            <p>{results[currentSlide].data.title}</p>
+            <p>{results[currentSlide].data.description[0].text}</p>
           </Texts>
         </Container>
       )
@@ -53,62 +56,46 @@ function Slider({ bannerData, isLoading }) {
 }
 
 Slider.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
   bannerData: PropTypes.shape({
-    license: PropTypes.string,
-    next_page: PropTypes.string,
-    page: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-    ]),
-    prev_page: PropTypes.string,
     results: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.string,
       uid: PropTypes.string,
       url: PropTypes.string,
       type: PropTypes.string,
       href: PropTypes.string,
-      tags: [],
+      tags: PropTypes.arrayOf(PropTypes.string),
       first_publication_date: PropTypes.string,
       last_publication_date: PropTypes.string,
       slugs: PropTypes.arrayOf(PropTypes.string),
       linked_documents: PropTypes.arrayOf(PropTypes.string),
       lang: PropTypes.string,
-      alternate_languages: [
-        {
-          id: PropTypes.string,
-          type: PropTypes.string,
-          lang: PropTypes.string,
-        },
-      ],
-      data: {
+      alternate_languages: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string,
+        type: PropTypes.string,
+        lang: PropTypes.string,
+      })),
+      data: PropTypes.shape({
         title: PropTypes.string,
-        description: [
-          {
-            type: PropTypes.string,
-            text: PropTypes.string,
-            spans: PropTypes.arrayOf(PropTypes.string),
-          },
-        ],
+        description: PropTypes.arrayOf(PropTypes.shape({
+          type: PropTypes.string,
+          text: PropTypes.string,
+          spans: PropTypes.arrayOf(PropTypes.string),
+        })),
         cta_link: PropTypes.string,
         cta_target: PropTypes.string,
-        main_image: {
-          dimensions: {
+        main_image: PropTypes.shape({
+          dimensions: PropTypes.shape({
             width: PropTypes.number,
             height: PropTypes.number,
-          },
+          }),
           alt: PropTypes.string,
-          copyright: null,
+          copyright: PropTypes.string,
           url: PropTypes.string,
-        },
-      },
+        }),
+      }),
     })),
-    results_per_page: PropTypes.number,
-    results_size: PropTypes.number,
-    total_pages: PropTypes.number,
-    total_results_size: PropTypes.number,
-    version: PropTypes.string,
   }).isRequired,
-  isLoading: PropTypes.bool.isRequired,
 };
 
 export default Slider;
